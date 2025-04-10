e69e0154d6781c8130a769ddfb37d9696eb9cbad
NathanArunaaa <nathanaruna07@gmail.com>
2025-04-09 21:00:16 -0400

    remove unused import of useEffect in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useState } from 'react'
+import {  useState } from 'react'
 import Footer from './components/footer'
 export default function Home() {
   const [state, setState] = useState(false)
