19f6f480c39549e40dbd52c38ab3e69c59c213ea
NathanArunaaa <nathanaruna07@gmail.com>
2025-04-12 10:33:03 -0400

    fix: update foreground color in globals.css and refine 404 page styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,13 +8,13 @@ export default function notfound() {
             "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)",
         }}
       ></div>
-      <section className="bg-white min-h-screen">
+      <section className="relative z-10">
       <div className="min-h-screen mx-auto px-4 flex items-center justify-center">
         <div className="text-center space-y-6">
-          <h1 className="text-7xl tracking-tight font-[Sagona] lg:text-9xl text-primary-600">
-            404 error
+          <h1 className="text-7xl tracking-tight font-[Avenir] lg:text-9xl text-gray-400">
+            404
           </h1>
-          <p className="text-3xl tracking-tight  text-gray-900 md:text-4xl font-[Avenir]">
+          <p className="text-3xl tracking-tight  text-gray-400 md:text-4xl font-[Sagona]">
             PAGE NOT FOUND
           </p>
           
