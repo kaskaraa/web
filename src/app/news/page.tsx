04759691ddb80a320d58c378e8ec5713f5ad3395
NathanArunaaa <nathanaruna07@gmail.com>
2025-04-18 13:54:12 -0400

    refactor(News): remove Newscard component and related imports from News page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,24 +1,9 @@
 "use client";
-import Navbar from "../components/navbar";
-import Newscard from "../components/newscard";
 
 export default function News() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <section class="bg-gray-2  pt-20 pb-10 lg:pt-[120px] lg:pb-20">
-      <div class="container mx-auto">
-        <div class="flex flex-wrap -mx-4">
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-        <Newscard />
-      </div>
-      </div>
-      </section>
-
+     
     </div>
   );
   
